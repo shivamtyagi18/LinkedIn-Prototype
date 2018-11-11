9db1c3dc21155bc26d5aa3587ce87b85cbf9c274
Sarang <saranggrover4488@gmail.com>
2018-11-11 14:29:04 -0800

    database installed

---
+++ Backend/configDB/db.js
@@ -0,0 +1,38 @@
+// var mysql = require("mysql");
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   port: 8889,
+//   database: "HomeAway"
+// });
+
+// var mysql = require("mysql");
+// var pool = mysql.createPool({
+//   connectionLimit: 1000,
+//   port: "8889",
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "HomeAway"
+// });
+
+// pool.getConnection(function(err) {
+//   if (err) throw err;
+//   console.log("Database connected!");
+// });
+
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Database connected!");
+// });
+
+// module.exports = pool;
+var mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+var url = `mongodb://sarang4488:partner123@ds141514.mlab.com:41514/linkedin`;
+mongoose.connect(url);
+console.log("Database Connected mongo");
+
+module.exports = { mongoose };
